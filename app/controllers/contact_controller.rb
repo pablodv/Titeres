2264fbad31d7ebf805c9890902c7d408345c6c24
Pablo <pablo.moreiramora@gmail.com>
2012-03-13 09:39:00 -0300

    Fix mailer

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,5 +4,7 @@ class ContactController < ApplicationController
     @message = Message.new(params[:message])
 
     NotificationsMailer.new_message(@message).deliver if @message.valid?
+
+    redirect_to root_path
   end
 end
